437130e8c1603a772b2fe658b96403e3672261ef
John Doe <johndoe@example.com>
2024-09-11 15:23:12 +0300

    asd

--- resources/views/layouts/front.blade.php
+++ resources/views/layouts/front.blade.php
@@ -25,7 +25,6 @@
 
                 <form class="modal-form">
                     @csrf
-                    <input type="hidden" name="product_id" id="productIdInput" value="" />
                     <input type="text" placeholder="Имя" class="input" name="name" required />
                     <input type="tel" placeholder="+7 (___) ___ __ __" class="input" name="phone" required />
 
