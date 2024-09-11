adce54203625f083ba929cce488879293790eea3
John Doe <johndoe@example.com>
2024-09-11 15:19:53 +0300

    asd

--- resources/views/catalog_product.blade.php
+++ resources/views/catalog_product.blade.php
@@ -9,7 +9,8 @@
             <div class="container">
                 <div class="product-page-top">
                     <h1 class="h1 product-page__title">
-                        {{ $car_make->title }} {{ $car_model->title }} {{ $car->title }} {{ $car->generation }}
+                        Ремонтные пороги и арки для <br> {{ $car_make->title }} {{ $car_model->title }} {{ $car->title }}
+                        {{ $car->generation }}
                     </h1>
                     <p class="product-page__description">
                         {{ $car->description }}
